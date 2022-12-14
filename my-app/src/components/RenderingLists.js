aecda3484ef3609c919615dc0d00773b49a3dadc
Harry Hayden <h_man1992@hotmail.com>
2022-12-14 10:49:24 +0000

    add map method

--- my-app/src/components/RenderingLists.js
+++ my-app/src/components/RenderingLists.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Book from './Book'
 
 export default function RenderingLists() {
     const bookList = [
@@ -6,11 +7,29 @@ export default function RenderingLists() {
         'The Great Gatsby',
         'The Catcher In The Rye'
     ]
+
+    const books = [
+        {
+          title: 'To Kill A Mockingbird',
+          author: 'Harper Lee',
+          pages: 281
+        },
+        {
+          title: 'The Great Gatsby',
+          author: 'F. Scott Fitzgerald',
+          pages: 218
+        },
+        {
+          title: 'The Catcher in the Rye',
+          author: 'J. D Salinger',
+          pages: 234
+        }
+    ]
+
   return (
-    <div>
-        {bookList.map(book => {
-            return <h2>{book}</h2>
-        })}
-    </div>
+    
+      books.map(book => {
+        return <Book book={book} />
+      })
   )
 }
